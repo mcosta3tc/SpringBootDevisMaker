f1d7cd8076b5955e8b1297877f2cbb605144055e
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-03-20 01:53:01 +0100

    init connect to the db

---
+++ devismaker/src/test/java/com/sagres/devismaker/DevismakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagres.devismaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevismakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
