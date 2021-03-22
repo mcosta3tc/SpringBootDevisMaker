3fdb9e685e11eb648f240c7aff7461602a75b6a0
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-03-22 17:11:24 +0100

    Ilies

--- devismaker_back/src/main/java/com/example/demo/Controller/ClientController.java
+++ devismaker_back/src/main/java/com/example/demo/Controller/ClientController.java
@@ -1,47 +1,53 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Model.Client;
-import com.example.demo.Service.ClientService;
+import com.example.demo.Model.Product;
+import com.example.demo.Repository.ClientRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clients")
 public class ClientController {
-    public ClientController(ClientService clientService) {
-        this.clientService = clientService;
+
+    private ClientRepository repository;
+
+    public ClientController(ClientRepository repository) {
+        this.repository = repository;
     }
 
-    private final ClientService clientService;
-
-    @PostMapping
-    public ResponseEntity addClient(@RequestBody Client client) {
-        clientService.addClient(client);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @GetMapping("/clients")
+    public List<Client> getAllClient() {
+        return repository.findAll();
     }
 
-    @PutMapping
-    public ResponseEntity<Object> updateClient(@RequestBody Client client) {
-        clientService.updateClient(client);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Client>> getAllClient() {
-        return ResponseEntity.ok(clientService.getAllClients());
-    }
-
-    @GetMapping("/{name}")
-    public ResponseEntity<Client> getClientByName(@PathVariable String name) {
-        return ResponseEntity.ok(clientService.getClientByName(name));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteClient(@PathVariable String id) {
-        clientService.deleteClient(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    @PostMapping("/clients")
+    public Client addClient(@RequestBody Client client) {
+        Client clientSaved = repository.save(client);
+        return clientSaved;
     }
 }
+
+//    @PostMapping(value = "/Produits")
+//    public ResponseEntity<Void> ajouterProduit(@RequestBody Product product) {
+//
+//        Product productAdded = productDao.save(product);
+//
+//        if (productAdded == null) {
+//            return ResponseEntity.noContent().build();
+//        } else {
+//            URI location = ServletUriComponentsBuilder
+//                    .fromCurrentRequest()
+//                    .path("/{id}")
+//                    .buildAndExpand(productAdded.getId())
+//                    .toUri();
+//            return ResponseEntity.created(location).build();
+//        }
+//    }
